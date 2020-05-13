1853890adcfcd846c137fee6e14c0d09cc938937
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-13 16:28:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,8 @@ class Application
 
     time = Time.new
     time.hour
-    if time.hour < 12 ? resp.write "Good Morning!": resp.write "Good Afternoon!"
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
-    end
+
+    time.hour < 12 ? resp.write "Good Morning!": resp.write "Good Afternoon!"
 
     resp.finish
   end
