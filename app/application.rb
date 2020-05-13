acff973d0ea3719c17908820d88d1396491c9883
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-13 16:18:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,16 @@
 class App
 
+  def call(env)
+    time = Time.new
+    time.hour
+
+    if time.hour < 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+
+    resp.finish
+  end
+
 end
