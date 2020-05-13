028852404cbfe0e4014a6c27615615cc6e9bc9aa
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-13 16:23:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if time.hour < 12
       resp.write "Good Morning!"
     else
-      resp.write "Good Afternoon"
+      resp.write "Good Afternoon!"
     end
 
     resp.finish
